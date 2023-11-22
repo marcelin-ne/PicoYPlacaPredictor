ff849ebf38d22c8c2e67d822b9fce194798d9b70
marcelinne <marcela.montalvo@epn.edu.ec>
2023-11-21 23:14:37 -0500

    feat(predictor): implement basic 'can_drive' logic

---
+++ src/predictor.py
@@ -0,0 +1,15 @@
+# Create a file named pico_y_placa.py
+
+class Predictor:
+    def __init__(self, plate_number, date, time):
+        self.plate_number = plate_number
+        self.date = date
+        self.time = time
+
+    def can_drive(self):
+        # Implement the logic to check if the car can be on the road
+        #Return a true o false value for the firts test
+        if self.date == "2023-11-21" and  "07:00" <= self.time <= "09:30":
+                return True
+        else:
+            return False
