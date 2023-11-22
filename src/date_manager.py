e76e812c0827490f5eac1ba8007cd3781603a706
marcelinne <marcela.montalvo@epn.edu.ec>
2023-11-22 02:24:56 -0500

    feat(holiday_checker): implement HolidayChecker class
    Implement the HolidayChecker class for checking if a given date is a holiday.

--- src/date_manager.py
+++ src/date_manager.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime , date
+
 
 class DateParser:
     def __init__(self, date):
@@ -14,8 +15,25 @@ class DateParser:
             # Handle the case where the date string is not valid
             return "Invalid Date"
 
-# Example of usage
-date_parser = DateParser("2023-11-22")
-weekday = date_parser.get_weekday()
-print(f"The weekday for the date is: {weekday}")
+class HolidayChecker:
+    def __init__(self):
+        # Diccionario de días festivos
+        self.holidays = {
+            "NEW_YEAR": date(2023, 1, 2),
+            "CARNAVAL_1": date(2023, 2, 20),
+            "CARNAVAL_2": date(2023, 2, 21),
+            "GOOD_FRIDAY": date(2023, 4, 7),
+            "LABOR_DAY": date(2023, 5, 1),
+            "PICHINCHA_BATTLE": date(2023, 5, 26),
+            "FIRST_CRY_OF_INDEPENDENCE": date(2023, 8, 11),
+            "GUAYAQUIL_INDEPENDENCE": date(2023, 10, 9),
+            "DAY_OF_THE_DEAD": date(2023, 11, 2),
+            "CUENCA_INDEPENDENCE": date(2023, 11, 3),
+            "QUITO_FOUNDATION": date(2023, 12, 4),
+            "CHRISTMAS": date(2023, 12, 25)
+        }
+
+    def is_holiday(self, date_to_check):
+        # Verificar si la fecha dada es un día festivo
+        return date_to_check in self.holidays.values()
 
