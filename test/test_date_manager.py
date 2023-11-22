4c9b12849e988f3fb2e3d05afd00b5b781b08a0f
marcelinne <marcela.montalvo@epn.edu.ec>
2023-11-22 02:25:24 -0500

    test(holiday_checker): add test suite for HolidayChecker
    Add test functions to verify the correctness of the HolidayChecker class.

--- test/test_date_manager.py
+++ test/test_date_manager.py
@@ -1,9 +1,11 @@
 import sys
 import os
+from datetime import date
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.date_manager import DateParser
+from src.date_manager import DateParser , HolidayChecker
+
 
 def test_valid_date():
     date_parser = DateParser("2023-11-27")
@@ -14,3 +16,13 @@ def test_invalid_date():
     date_parser = DateParser("invalid_date")
     weekday = date_parser.get_weekday()
     assert weekday == "Invalid Date"
+
+def test_valid_holiday():
+    holiday_checker = HolidayChecker()
+    date_to_check = date(2023, 2, 20)  # CARNAVAL_1
+    assert holiday_checker.is_holiday(date_to_check) is True
+
+def test_non_holiday():
+    holiday_checker = HolidayChecker()
+    date_to_check = date(2023, 1, 3)  # Not a fetive date
+    assert holiday_checker.is_holiday(date_to_check) is False
